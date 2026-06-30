de7f359808dc6c39c6bfcfd40c347c85d03360fc
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 00:39:03 -0500

    ideclaramos variables y vista de

--- vistas/DialogoRegistrarLibro.java
+++ vistas/DialogoRegistrarLibro.java
@@ -1,5 +1,27 @@
 package vistas;
+import controladores.GestorBiblioteca;
+import estructuras.ExceptionIsEmpty;
+import estructuras.ItemDuplicated;
+import javax.swing.*;
+import java.awt.*;
 
-public class DialogoRegistrarLibro {
+public class DialogoRegistrarLibro extends JDialog {
+    private JTextField txtCodigo;
+    private JTextField txtTitulo;
+    private JTextField txtAutor;
+    private JTextField txtCategoria;
+    private JTextField txtAnio;
+    public DialogoRegistrarLibro(JFrame ventanaPadre, GestorBiblioteca gestor) {
+        super(ventanaPadre, "Registrar Nuevo Libro", true);
+        setSize(350, 300);
+        setLocationRelativeTo(ventanaPadre); // Centra este cuadro sobre la ventana principal
+        setLayout(new BorderLayout(10, 10));
+        JPanel panelFormulario = new JPanel(new GridLayout(5, 2, 10, 10));
+        panelFormulario.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        txtCodigo = new JTextField();
+        txtTitulo = new JTextField();
+        txtAutor = new JTextField();
+        txtCategoria = new JTextField();
+        txtAnio = new JTextField();
     
 }
