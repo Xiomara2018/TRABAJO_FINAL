21a3edc24118a67124782093559aa05f1c77e3bc
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 00:22:39 -0500

    corregimos los botones

--- vistas/VentanaPrincipal.java
+++ vistas/VentanaPrincipal.java
@@ -9,29 +9,31 @@ public class VentanaPrincipal extends JFrame
 public VentanaPrincipal() {
     gestor = new GestorBiblioteca();
     setTitle("QuickLibrary - Sistema de Gestión");
-    setSize(400, 350);
+    setSize(600, 450);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setLocationRelativeTo(null);
-    setLayout(new GridLayout(5, 1, 10, 10));
+    setLayout(new BorderLayout(10, 10));
 
-    JButton btnRegistrarLibro = new JButton("1. Registrar Libro");
-    JButton btnMostrarLibros = new JButton("2. Mostrar Todos los Libros");
-    JButton btnRegistrarSolicitud = new JButton("7. Registrar Solicitud de Préstamo");
-    btnRegistrarLibro.addActionListener(e -> {
-        JOptionPane.showMessageDialog(this, "Próximamente: Formulario de Registro");
-    });
-    btnMostrarLibros.addActionListener(e -> {
-        JOptionPane.showMessageDialog(this, "Próximamente: Tabla de libros");
-    });
-    add(new JLabel("Bienvenido a QuickLibrary", SwingConstants.CENTER));
-    add(btnRegistrarLibro);
-    add(btnMostrarLibros);
-    add(btnRegistrarSolicitud);
-    }
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-        new VentanaPrincipal().setVisible(true);
-        });
-    }
+    JLabel lblTitulo = new JLabel("SISTEMA DE GESTIÓN QUICKLIBRARY", SwingConstants.CENTER);
+    lblTitulo.setFont(new Font("Arial", Font.BOLD, 18));
+    lblTitulo.setBorder(BorderFactory.createEmptyBorder(15, 0, 15, 0)); // Márgenes
+    add(lblTitulo, BorderLayout.NORTH);
 
-}
+    JPanel panelBotones = new JPanel();
+    panelBotones.setLayout(new GridLayout(6, 2, 10, 10));
+    panelBotones.setBorder(BorderFactory.createEmptyBorder(10, 20, 20, 20));
+
+
+    JButton btn1 = new JButton("1. Registrar libro");
+    JButton btn2 = new JButton("2. Mostrar libros");
+    JButton btn3 = new JButton("3. Buscar libro por código");
+    JButton btn4 = new JButton("4. Buscar libros por categoría");
+    JButton btn5 = new JButton("5. Modificar libro");
+    JButton btn6 = new JButton("6. Eliminar libro");
+    JButton btn7 = new JButton("7. Registrar solicitud de préstamo");
+    JButton btn8 = new JButton("8. Mostrar cola de solicitudes");
+    JButton btn9 = new JButton("9. Atender siguiente solicitud");
+    JButton btn10 = new JButton("10. Registrar devolución");
+    JButton btn11 = new JButton("11. Mostrar reporte");
+    JButton btn12 = new JButton("12. Salir");
+
