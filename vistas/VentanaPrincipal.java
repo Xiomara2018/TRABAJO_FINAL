4e413ce815477b1a089750e6aef669e2f8d3152e
Xiomara Nuñez <xiomaranunezccupa@gmail.com>
2026-06-30 00:11:40 -0500

    titulo

--- vistas/VentanaPrincipal.java
+++ vistas/VentanaPrincipal.java
@@ -3,7 +3,12 @@ package vistas;
 import controladores.GestorBiblioteca;
 import javax.swing.*;
 import java.awt.*;
-public class VentanaPrincipal {
+public class VentanaPrincipal extends JFrame
+ {private GestorBiblioteca gestor;
 
-    
+public VentanaPrincipal() {
+    gestor = new GestorBiblioteca();
+    setTitle("QuickLibrary - Sistema de Gestión");
+        setSize(400, 350);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
