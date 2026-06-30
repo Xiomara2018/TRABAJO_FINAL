e1942cbfa8d920fd5833ee52c42b9a297b163d69
MrChocman <kirbymanuel303@gmail.com>
2026-06-29 22:30:19 -0500

    modificaciones finales

--- estructuras/Cola.java
+++ estructuras/Cola.java
@@ -15,7 +15,7 @@ public class Cola<T> {
         return first == null; //devolvera true si esta vacia
     }
 
-    public void enqueque(T x){ //Insertar elemento
+    public void enqueue(T x){ //Insertar elemento
         NodeQueue<T> newnode = new NodeQueue<>(x); //creacion de nuevo nodo
         if (IsEmpty()){  //si esta vacio los nodos first y last se les asignara el valor de newnode
             first = newnode;
@@ -30,9 +30,9 @@ public class Cola<T> {
         size++; //a cada insercion el tamaño de la cola incrementara +1
     }
 
-    public T dequeue(){ //sacar de la cola 
+    public T dequeue() throws ExceptionIsEmpty{ //sacar de la cola 
         if(IsEmpty()){ //si la cola esta vacia se mandara un mensaje
-            System.out.println("La cola de solicitudes esta vacia");
+            throw new ExceptionIsEmpty("La cola de solicitudes esta vacia");
         }
         T aux = this.first.getdata(); //se obtendra el primer dato con la variable auxiliar aux
         this.first = this.first.next; //y ahora el primero vendra a ser el siguiente del primero, desencolando el primer nodo de la cola
@@ -44,30 +44,37 @@ public class Cola<T> {
         return aux; //retornara el valor auxiliar
     }
 
-    public T peek(){ 
-        T daten = this.first.getdata();
-        if (daten == null){
-            return null;
+    public T peek() throws ExceptionIsEmpty{ //deja ver el primero en la cola sin desencolarlo
+        if(IsEmpty()){
+            throw new ExceptionIsEmpty("La cola de solicitudes esta vacia");
         }
-        return daten;
+        return this.first.getdata();
     }
 
     public int size(){
         return size;
     }
 
-    public void mostrar(){
+    public String mostrar(){
+        if(IsEmpty()){
+            return "La cola de solicitudes está vacía.";
+        }
+        
+        StringBuilder textoCola = new StringBuilder();
+        textoCola.append("Cola graficada: \n");
+        
         NodeQueue<T> rec = first;
         
-        if(IsEmpty()){
-            System.out.println("La cola de solicitudes esta vacia");
+        while (rec != null){ // Corregido: antes decía rec.next != null y se comía el último
+            textoCola.append(rec.getdata().toString());
+            if(rec.getnext() != null){
+                textoCola.append(" -> ");
+            }
+            rec = rec.getnext();
         }
-        System.out.print("Cola graficada");
-        while (rec.next != null){
-            System.out.print(rec.getdata() + " -> ");
-            rec = rec.next;
-        }
+        
+        return textoCola.toString();
 
-    }
+    
     
 }
